da6d2eb76f9f064a2a0d8b5aa7c741d5d65be4d8
TejasBaid <tejasbaid3@gmail.com>
2025-04-09 19:27:27 +0530

    Fixed pwa issue

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const withPWA = nextPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    disable: false,
+    disable: process.env.NODE_ENV === 'development', // Optional: Disable PWA in dev mode
 });
 const nextConfig = {
     reactStrictMode: true,
